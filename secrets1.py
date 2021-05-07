9cee301f5375b8a708e3fc541c4dd1a272f2adcf
oldblackcrow <45359956+oldblackcrow@users.noreply.github.com>
2021-05-07 09:59:56 -0400

    Add files via upload

---
+++ secrets1.py
@@ -0,0 +1,13 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid' : 'YOUR-NETWORK-ID',
+    'password' : 'NETWORK-ID-PASSWORD',
+    'timezone' : "America/New_York", # http://worldtimeapi.org/timezones
+    'aio_username' : 'YOUR_USERNAME',
+    'aio_key' : 'YOUR_AIO_KEY',
+    'broker' : 'https://io.adafruit.com',
+    'port':8883
+    }
+# Write your code here :-)
